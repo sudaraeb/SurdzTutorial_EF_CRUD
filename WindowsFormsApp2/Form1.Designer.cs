36f686a02c8a16be9659a7e585a57eefecec8f1b
ERICSON BOSTON SUDARA <32215019+sudaraeb@users.noreply.github.com>
2019-03-09 20:24:17 +0800

    Add files via upload

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentID.Location = new System.Drawing.Point(159, 29);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(517, 29);
+            this.txtStudentID.TabIndex = 1;
+            // 
+            // txtFName
+            // 
+            this.txtFName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFName.Location = new System.Drawing.Point(159, 64);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(517, 29);
+            this.txtFName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name";
+            // 
+            // txtMName
+            // 
+            this.txtMName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMName.Location = new System.Drawing.Point(159, 102);
+            this.txtMName.Name = "txtMName";
+            this.txtMName.Size = new System.Drawing.Size(517, 29);
+            this.txtMName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Middle Name";
+            // 
+            // txtLName
+            // 
+            this.txtLName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLName.Location = new System.Drawing.Point(159, 137);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(517, 29);
+            this.txtLName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Last Name";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(389, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(488, 453);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(590, 453);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 33);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 189);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(648, 249);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 513);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "EF CRUD APPLICATION";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           //ADD 
+            tbl_students _student = new tbl_students();
+            _student.StudentID = txtStudentID.Text;
+            _student.FirstName = txtFName.Text;
+            _student.LastName = txtLName.Text;
+            _student.MiddleName = txtMName.Text;
+
+            using (SampleEntities db = new SampleEntities())
+            {
+                db.tbl_students.Add(_student);
+                db.SaveChanges();
+            }
+            MessageBox.Show("Add Successful");
+            DisplayRecords();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayRecords();
+        }
+         void DisplayRecords()
+        {
+
+            tbl_students _student = new tbl_students();
+            using (SampleEntities db = new SampleEntities())
+            {
+                var stud = db.tbl_students.ToList();
+                dataGridView1.DataSource = stud;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Update
+            tbl_students _student = new tbl_students();
+            _student.StudentID = txtStudentID.Text;
+            _student.FirstName = txtFName.Text;
+            _student.MiddleName = txtMName.Text;
+            _student.LastName = txtLName.Text;
+
+            using (SampleEntities db = new SampleEntities())
+            {
+                var stud = db.tbl_students.FirstOrDefault(a => a.StudentID == _student.StudentID);
+                if (stud != null)
+                {
+                    stud.FirstName = _student.FirstName;
+                    stud.LastName = _student.LastName;
+                    stud.MiddleName = _student.MiddleName;
+                    db.SaveChanges();
+                }
+                MessageBox.Show("Update  Successful");
+                DisplayRecords();
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //DISPLAY RECORDS TO DATAGRIDVIEW
+            try
+            {
+                txtStudentID.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtFName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtMName.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtLName.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            }
+            catch (Exception)
+            {
+
+            }
+         
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tbl_students _student = new tbl_students();
+            _student.StudentID = txtStudentID.Text;
+            using (SampleEntities db = new SampleEntities())
+            {
+                var stud = db.tbl_students.FirstOrDefault(a => a.StudentID == _student.StudentID);
+                db.tbl_students.Remove(stud);
+                db.SaveChanges();
+            }
+            MessageBox.Show("Delete  Successful");
+            DisplayRecords();
+        }
+
+    }
+}
